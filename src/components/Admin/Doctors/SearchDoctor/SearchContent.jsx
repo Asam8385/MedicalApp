bb5485ce3f8422b2b3cdeab21a504cba4395b3b5
Asam <subairasam8733260@gmail.com>
2024-03-11 10:53:01 +0530

    change the login in attribute

--- src/components/Admin/Doctors/SearchDoctor/SearchContent.jsx
+++ src/components/Admin/Doctors/SearchDoctor/SearchContent.jsx
@@ -24,7 +24,7 @@ const SearchContent = ({ data }) => {
                     <div className="doc-info">
                         <h5 className='mb-0'><Link to={`/doctors/profile/${data?.id}`}>Dr. {data?.firstName + ' ' + data?.lastName}</Link></h5>
                         <p className='m-0 form-text'>{data?.designation}</p>
-                        <p className="doc-department m-0"><img src={showImg} className="img-fluid" alt="Speciality" />Urology</p>
+                        <p className="doc-department m-0"><img src={showImg} className="img-fluid" alt="Speciality" />{data?.specialization}</p>
 
                         <div className='d-flex align-items-center'>
                             <div>
