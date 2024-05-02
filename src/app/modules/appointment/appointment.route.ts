9ad05f2a9630a6b561a0910114f5f77dcfcc6317
Asam <subairasam8733260@gmail.com>
2024-05-02 20:03:32 +0530

    vercel hosting failed

--- src/app/modules/appointment/appointment.route.ts
+++ src/app/modules/appointment/appointment.route.ts
@@ -9,10 +9,10 @@ router.get('/', AppointmentController.getAllAppointment);
 
 router.get('/patient/appointments',auth(AuthUser.PATIENT), AppointmentController.getPatientAppointmentById);
 router.get('/patient/invoices',auth(AuthUser.PATIENT), AppointmentController.getPatientPaymentInfo);
-router.get('/doctor/invoices',auth(AuthUser.DOCTOR), AppointmentController.getDoctorInvoices);
+router.get('/doctor/invoices',auth(AuthUser.DOCTOR, AuthUser.ADMIN ), AppointmentController.getDoctorInvoices);
 
-router.get('/doctor/appointments',auth(AuthUser.DOCTOR), AppointmentController.getDoctorAppointmentsById);
-router.get('/doctor/patients',auth(AuthUser.DOCTOR), AppointmentController.getDoctorPatients);
+router.get('/doctor/appointments',auth(AuthUser.DOCTOR, AuthUser.ADMIN), AppointmentController.getDoctorAppointmentsById);
+router.get('/doctor/patients',auth(AuthUser.DOCTOR, AuthUser.ADMIN), AppointmentController.getDoctorPatients);
 
 router.get('/patient-payment-info/:id',auth(AuthUser.PATIENT, AuthUser.DOCTOR), AppointmentController.getPaymentInfoViaAppintmentId);
 
@@ -25,7 +25,7 @@ router.get('/:id', AppointmentController.getAppointment);
 router.delete('/:id', AppointmentController.deleteAppointment);
 router.patch('/:id', auth(AuthUser.ADMIN, AuthUser.DOCTOR, AuthUser.PATIENT),AppointmentController.updateAppointment);
 //doctor side
-router.patch('/doctor/update-appointment',auth(AuthUser.DOCTOR), AppointmentController.updateAppointmentByDoctor);
+router.patch('/doctor/update-appointment',auth(AuthUser.DOCTOR, AuthUser.ADMIN), AppointmentController.updateAppointmentByDoctor);
 
 
 export const AppointmentRouter = router;
